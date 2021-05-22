699f3444892763083aad78428dc9d1ff5b33a0ad
千橘 雫霞 <i@qianjunakasumi.ren>
2021-05-22 13:57:22 +0800

    feat: package `openDatabase` function

---
+++ lib/common/sqflite.dart
@@ -0,0 +1,15 @@
+import 'package:sqflite/sqflite.dart';
+
+const String _name = 'lyexamination';
+const String _createTable =
+    'CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT)';
+
+Future<Database> appDatabase() {
+  return openDatabase(
+    _name,
+    onCreate: (db, v) {
+      return db.execute(_createTable);
+    },
+    version: 1,
+  );
+}
