65491cea61cfbd8236ac6802ea7d07c01eccefde
千橘 雫霞 <i@qianjunakasumi.ren>
2021-05-22 21:29:37 +0800

    refactor: rename `MyApp` to `LYExaminationApp`

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:lyexamination/home.dart';
 import 'package:lyexamination/welcome.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  runApp(LYExaminationApp());
 }
 
-class MyApp extends StatelessWidget {
+class LYExaminationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
