6fa5a9c7e07e3564e9ecb4f0280a943a707d08f4
千橘 雫霞 <i@qianjunakasumi.ren>
2021-05-26 23:41:15 +0800

    refactor: beautify login buttons & explicit tips of `welcomePage`

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -3,6 +3,64 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class WelcomePage extends StatelessWidget {
+  static final ButtonStyle _style = ButtonStyle(
+    elevation: MaterialStateProperty.all(0),
+    shape: MaterialStateProperty.all(
+      RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+    ),
+    padding: MaterialStateProperty.all(
+      EdgeInsets.only(top: 12, left: 32, right: 32, bottom: 12),
+    ),
+  );
+
+  final Widget _passwordLogin = ElevatedButton(
+    onPressed: () => print('密码'),
+    style: _style,
+    child: ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 200),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.lock_outline_rounded),
+              Text(
+                '密码登录',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+          Icon(Icons.keyboard_arrow_right_outlined),
+        ],
+      ),
+    ),
+  );
+
+  final Widget _wechatLogin = OutlinedButton(
+    onPressed: () => print('微信'),
+    style: _style,
+    child: ConstrainedBox(
+      constraints: BoxConstraints(maxWidth: 200),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Icon(Icons.message_outlined),
+              Text('微信登录'),
+            ],
+          ),
+          Icon(Icons.keyboard_arrow_right_outlined),
+        ],
+      ),
+    ),
+  );
+
+  static const _license =
+      'https://github.com/qianjunakasumi/LYExamination/blob/main/LICENSE';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +71,11 @@ class WelcomePage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
-                margin: EdgeInsets.only(bottom: 16),
+                margin: EdgeInsets.only(bottom: 8),
                 child: Image.asset('assets/lighthouse.png', width: 200),
               ),
               Container(
-                margin: EdgeInsets.only(bottom: 48),
+                margin: EdgeInsets.only(bottom: 32),
                 child: Text(
                   '龙岩考试',
                   textScaleFactor: 2,
@@ -27,50 +85,22 @@ class WelcomePage extends StatelessWidget {
                   ),
                 ),
               ),
-              OutlinedButton.icon(
-                onPressed: () => print('密码'),
-                icon: Icon(Icons.lock_rounded),
-                label: Text('密码登录'),
-                style: ButtonStyle(
-                  padding: MaterialStateProperty.all(
-                    EdgeInsets.only(
-                      top: 12,
-                      left: 88,
-                      right: 88,
-                      bottom: 12,
-                    ),
-                  ),
-                ),
-              ),
+              _passwordLogin,
               Container(
-                margin: EdgeInsets.only(top: 16, bottom: 32),
-                child: OutlinedButton.icon(
-                  onPressed: () => print('微信'),
-                  icon: Icon(Icons.chat_rounded),
-                  label: Text('微信登录'),
-                  style: ButtonStyle(
-                    padding: MaterialStateProperty.all(
-                      EdgeInsets.only(
-                        top: 12,
-                        left: 88,
-                        right: 88,
-                        bottom: 12,
-                      ),
-                    ),
-                  ),
-                ),
+                margin: EdgeInsets.only(top: 8, bottom: 32),
+                child: _wechatLogin,
               ),
               RichText(
                 text: TextSpan(
-                  style: TextStyle(color: Colors.black),
+                  style: TextStyle(color: Colors.black45, fontSize: 12),
                   children: [
-                    TextSpan(text: '登录即代表您已同意'),
+                    TextSpan(text: '登录即代表您已理解'),
                     TextSpan(
-                      text: '《使用条款》',
+                      text: '《开源软件许可证》',
                       style: TextStyle(color: Colors.blue),
                       recognizer: TapGestureRecognizer()
                         ..onTap = () {
-                          launch('https://github.com/');
+                          launch(_license);
                         },
                     ),
                   ],
