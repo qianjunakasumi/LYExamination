e796722cef7aaacd8ee906e31e4049cb51ee8fa6
千橘 雫霞 <i@qianjunakasumi.ren>
2021-05-27 23:01:40 +0800

    refactor: constructed in function

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -3,66 +3,71 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class WelcomePage extends StatelessWidget {
-  static final ButtonStyle _style = ButtonStyle(
-    elevation: MaterialStateProperty.all(0),
-    shape: MaterialStateProperty.all(
-      RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-    ),
-    padding: MaterialStateProperty.all(
-      EdgeInsets.only(top: 12, left: 32, right: 32, bottom: 12),
-    ),
-  );
-
-  final Widget _passwordLogin = ElevatedButton(
-    onPressed: () => print('密码'),
-    style: _style,
-    child: ConstrainedBox(
-      constraints: BoxConstraints(maxWidth: 200),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Icon(Icons.lock_outline_rounded),
-              Text(
-                '密码登录',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-          Icon(Icons.keyboard_arrow_right_outlined),
-        ],
-      ),
-    ),
-  );
-
-  final Widget _wechatLogin = OutlinedButton(
-    onPressed: () => print('微信'),
-    style: _style,
-    child: ConstrainedBox(
-      constraints: BoxConstraints(maxWidth: 200),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset('assets/icons/wechat.png', width: 20),
-              Text('微信登录'),
-            ],
-          ),
-          Icon(Icons.keyboard_arrow_right_outlined),
-        ],
-      ),
-    ),
-  );
-
-  static const _license =
+  static const String _license =
       'https://github.com/qianjunakasumi/LYExamination/blob/main/LICENSE';
+
+
+
+  void _next(BuildContext context, String m) {
+  }
 
   @override
   Widget build(BuildContext context) {
+    final ButtonStyle _style = ButtonStyle(
+      elevation: MaterialStateProperty.all(0),
+      shape: MaterialStateProperty.all(
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
+      ),
+      padding: MaterialStateProperty.all(
+        EdgeInsets.only(top: 12, left: 32, right: 16, bottom: 12),
+      ),
+    );
+
+    final Widget _passwordLogin = ElevatedButton(
+      onPressed: () => _next(context, 'password'),
+      style: _style,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 200),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Icon(Icons.lock_outline_rounded),
+                Text(
+                  '密码登录',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Icon(Icons.keyboard_arrow_right_outlined),
+          ],
+        ),
+      ),
+    );
+
+    final Widget _wechatLogin = OutlinedButton(
+      onPressed: () => _next(context, 'wechat'),
+      style: _style,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 200),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset('assets/icons/wechat.png', width: 20),
+                Text('微信登录'),
+              ],
+            ),
+            Icon(Icons.keyboard_arrow_right_outlined),
+          ],
+        ),
+      ),
+    );
+
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
