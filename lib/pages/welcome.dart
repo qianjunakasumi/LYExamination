80a84df1019a6c1c88a77385b90a2d9622812d9a
千橘 雫霞 <i@qianjunakasumi.ren>
2021-06-05 20:34:09 +0800

    refactor: kill wechat fucking login

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -1,98 +1,96 @@
 import 'package:flutter/material.dart';
-import 'package:lyexamination/dao/student.dart';
-import 'package:lyexamination/model/database/table/student.dart';
-import 'package:lyexamination/pages/terms.dart';
+import 'package:lyexamination/boot//app/config.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WelcomePage extends StatelessWidget {
-  void _skip(BuildContext context, List<Student> stu) {
-    if (stu.length != 0) {
-      // Navigator.pushNamedAndRemoveUntil(context, '/home', (route) => false);
-      // TODO 登录
-    }
-  }
-
-  void _next(BuildContext context, String m) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (BuildContext context) => TermsPage(m)),
-    );
-  }
-
-  final ButtonStyle _btnStyle = ButtonStyle(
-    elevation: MaterialStateProperty.all(0),
-    padding: MaterialStateProperty.all(
-      EdgeInsets.only(top: 12, left: 32, right: 16, bottom: 12),
-    ),
-    shape: MaterialStateProperty.all(
-      RoundedRectangleBorder(borderRadius: BorderRadius.circular(32)),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
-    retrieveStudents().then((stu) => _skip(context, stu));
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset('assets/background/welcome.png', width: 264),
-              SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: () => _next(context, 'password'),
-                style: _btnStyle,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 216),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Icon(Icons.lock_outline_rounded),
-                          Text(
-                            '密码登录',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
+        child: Stack(
+          children: [
+            ListView(
+              padding: EdgeInsets.only(left: 24, right: 24, bottom: 80),
+              children: [
+                SizedBox(height: 128),
+                Image.asset('assets/background/welcome.png', height: 128),
+                SizedBox(height: 128),
+                Center(
+                  child: Text(
+                    '龙岩考试',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                    textScaleFactor: 2.16,
+                  ),
+                ),
+                SizedBox(height: 4),
+                Divider(),
+                SizedBox(height: 24),
+                Text('理解您要跳过这些内容，但是为了维护您的权益，仍请花些时间了解'),
+                SizedBox(height: 24),
+                Text('谁提供服务', textScaleFactor: 1.32),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () => launch(config.githubAddr),
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(0),
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.only(top: 12, bottom: 12),
+                    ),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
                       ),
-                      Icon(Icons.keyboard_arrow_right_outlined),
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('服务提供方：', textScaleFactor: 1.2),
+                      Text('${config.sponsor} <${config.email}>'),
                     ],
                   ),
                 ),
-              ),
-              SizedBox(height: 12),
-              OutlinedButton(
-                onPressed: () => _next(context, 'wechat'),
-                style: _btnStyle,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(maxWidth: 216),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Image.asset(
-                            'assets/icons/wechat.png',
-                            width: 24,
-                            height: 24,
-                          ),
-                          Text('微信登录'),
-                        ],
+                SizedBox(height: 8),
+                Text('服务器：' + config.apiAddr),
+                Divider(color: Colors.grey),
+                Text(
+                  '我们倡导自由软件运动，因此每个人都可以为您提供服务。有且仅有 “api-lyexamination.qianjunakasumi.ren”（官方）服务器受保护。不受保护的服务器可能存储、披露或贩卖您的个人信息。',
+                ),
+                SizedBox(height: 16),
+                Text('著作权信息', textScaleFactor: 1.32),
+                SizedBox(height: 16),
+                Text('Copyright (c) 2021-现在 千橘 雫霞'),
+                SizedBox(height: 16),
+                Text('许可证', textScaleFactor: 1.32),
+                SizedBox(height: 16),
+                Text('Mozilla 公共许可证 2.0（Mozilla Public License 2.0）'),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                margin: EdgeInsets.only(bottom: 16),
+                child: ElevatedButton(
+                  onPressed: () => Navigator.pushNamed(context, '/create'),
+                  style: ButtonStyle(
+                    padding: MaterialStateProperty.all(
+                      EdgeInsets.only(top: 12, left: 88, right: 88, bottom: 12),
+                    ),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(32),
                       ),
-                      Icon(Icons.keyboard_arrow_right_outlined),
-                    ],
+                    ),
+                  ),
+                  child: Text(
+                    '我已阅读',
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
