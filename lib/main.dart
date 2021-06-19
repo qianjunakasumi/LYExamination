d289df9913a079ae6544149e4e07cd0bcdb35218
千橘 雫霞 <i@qianjunakasumi.ren>
2021-06-19 21:36:14 +0800

    feat: more beautiful status bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lyexamination/pages/home.dart';
 import 'package:lyexamination/pages/welcome.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(LYExaminationApp());
+  soak();
+}
+
+/// 沉浸式状态栏
+void soak() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor: Color(0xFF000000),
+      systemNavigationBarDividerColor: null,
+      statusBarColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+      statusBarBrightness: Brightness.light,
+    ),
+  );
 }
 
 class LYExaminationApp extends StatelessWidget {
