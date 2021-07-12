6382410b8bb4ff7760672d152a443c612d81a425
千橘 雫霞 <i@qianjunakasumi.ren>
2021-07-13 00:50:04 +0800

    feat✨: `ProfileModel`

--- lib/model/profile.dart
+++ lib/model/profile.dart
@@ -4,3 +4,13 @@ class AccountModel {
 
   AccountModel({this.phone, this.password});
 }
+
+class ProfileModel {
+  final String number;
+  final String name;
+  final String school;
+  final String grade;
+  final String classNum;
+
+  ProfileModel(this.number, this.name, this.school, this.grade, this.classNum);
+}
