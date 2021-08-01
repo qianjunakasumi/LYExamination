260d5c972f9c6b77c294fbbaa75e910f4ff3b4fa
千橘 雫霞 <i@qianjunakasumi.ren>
2021-08-01 15:23:59 +0800

    refactor♻️: observe `MaterialApp` theme
    取消沉浸状态栏

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:lyexamination/boot/network.dart' as network;
 import 'package:lyexamination/router.dart';
 
 void main() async {
-  soak();
   network.init();
   runApp(LYExaminationApp());
 }
@@ -13,7 +11,6 @@ void main() async {
 class LYExaminationApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     // TODO 读取档案信息，若存在档案 -> initialRoute = '/welcome'
     // TODO              不存在   -> initialRoute = '/privacy'
 
@@ -23,16 +20,4 @@ class LYExaminationApp extends StatelessWidget {
       getPages: routes,
     );
   }
-}
-
-/// 沉浸式状态栏
-void soak() async {
-  SystemChrome.setSystemUIOverlayStyle(
-    SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.transparent,
-      systemNavigationBarIconBrightness: Brightness.dark,
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.dark,
-    ),
-  );
 }
