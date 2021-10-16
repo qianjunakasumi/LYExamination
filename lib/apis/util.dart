73d52d8d199f533f604e28a4aeb1b807d29b161c
千橘 雫霞 <i@qianjunakasumi.ren>
2021-10-16 16:04:58 +0800

    refactor🎨: avoid using `forEach` with a function literal

--- lib/apis/util.dart
+++ lib/apis/util.dart
@@ -1,5 +1,7 @@
 List<T> dynamicToTypeList<T>(List<dynamic> o, T Function(dynamic) f) {
   List<T> l = [];
-  o.forEach((d) => l.add(f(d)));
+  for (var d in o) {
+    l.add(f(d));
+  }
   return l;
 }
