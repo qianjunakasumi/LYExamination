bffd78ff57c9e71aa6cece88220b1c393fd12a64
千橘 雫霞 <i@qianjunakasumi.ren>
2021-10-24 11:00:03 +0800

    refactor♻: 编译时信息

---
+++ lib/boot/config.dart
@@ -0,0 +1,13 @@
+const maintainer = String.fromEnvironment('maintainer', defaultValue: '未署名');
+
+const email =
+    String.fromEnvironment('email', defaultValue: 'undefined@example.foo');
+
+const github = String.fromEnvironment('github',
+    defaultValue:
+        'https://github.com/lyexamination-undefined-maintainer-github-profile/undefined');
+
+const maintainerGitHubProfile = String.fromEnvironment(
+    'maintainerGitHubProfile',
+    defaultValue:
+        'https://github.com/lyexamination-undefined-maintainer-github-profile');
