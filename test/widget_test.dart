f545e588a6bc889d4cd280ec127711f080ee064c
千橘 雫霞 <i@qianjunakasumi.ren>
2022-01-31 18:14:53 +0800

    perf⚡️(flutter-sdk): 升级 Flutter 至 2.8.1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lyexamination/main.dart';
+
 void main() {
-  /*
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App running test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const LYExaminationApp());
-
+    /*
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
@@ -22,6 +24,6 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
+    */
   });
-  */
 }
