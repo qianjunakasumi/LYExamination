ab564d3eecbebb1fed2f1ade0ae633486e9c7d57
千橘 雫霞 <i@qianjunakasumi.ren>
2022-02-01 19:49:21 +0800

    chore📄: 版权通知
    close #17

--- lib/data/apis/accounts/roles/get/std.dart
+++ lib/data/apis/accounts/roles/get/std.dart
@@ -1,3 +1,11 @@
+//  Copyright (c) 2022 qianjunakasumi <i@qianjunakasumi.ren>
+//                     qianjunakasumi <qianjunakasumi@outlook.com>
+//                     [qianjunakasumi] 千橘雫霞(https://github.com/qianjunakasumi)
+//
+//       This Source Code Form is subject to the terms of the Mozilla Public
+//       License, v. 2.0. If a copy of the MPL was not distributed with this
+//       file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import '/data/apis/util.dart';
 
 class APIACCNTsRolesGetReq {
--- lib/data/apis/service.dart
+++ lib/data/apis/service.dart
@@ -1,3 +1,11 @@
+//  Copyright (c) 2022 qianjunakasumi <i@qianjunakasumi.ren>
+//                     qianjunakasumi <qianjunakasumi@outlook.com>
+//                     [qianjunakasumi] 千橘雫霞(https://github.com/qianjunakasumi)
+//
+//       This Source Code Form is subject to the terms of the Mozilla Public
+//       License, v. 2.0. If a copy of the MPL was not distributed with this
+//       file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_cookie_manager/dio_cookie_manager.dart';
